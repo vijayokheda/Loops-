606f8bed71dc40cd5d1a095cb93c54ce4fbf08c8
vijayokheda <144098485+vijayokheda@users.noreply.github.com>
2024-12-12 19:44:49 +0530

    Add files via upload

---
+++ Loops/main.py
@@ -0,0 +1,58 @@
+#      Counting solutions
+
+# Print table
+
+# number = 3 
+
+# for i in range(1,11):
+#     # if i == 5:
+#     #     continue
+#     print(number,'x',i, '=',number*i)
+
+    
+# giving the even number 
+
+# n = 10
+# sum_even = 0
+
+
+# for i in range(1,n+1):
+#     if i%2 == 0:
+#         sum_even += 1
+# print("Sum of even number is")
+
+
+# 
+#     Reversing Number
+
+# input_str = "vijay okheda" 
+
+
+
+#       factorial Number 
+
+
+# number = 5
+# factorial = 1
+
+
+# while number > 0:
+#     factorial = factorial * number
+#     number = number - 1
+
+# print("Factroial:",factorial) 
+
+
+
+# while True:
+#   number = int(input("Enter the vaule b/w 1 and 10"))
+#   if 1 <= number <=10:
+#     print("thanks")
+#     break
+#   else:
+#     print("Invalid number, try again")
+     
+
+
+
+     
